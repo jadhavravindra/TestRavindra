73b9b656c0f338e2587cb9168a202552642d74b8
Ravindra Jadhav <ravindrajadhav841@gmail.com>
2021-07-13 08:02:35 +0530

    branch 2 commit for new changes conflict

---
+++ src/ExceptionClasses/Atest.java
@@ -0,0 +1,9 @@
+package ExceptionClasses;
+
+public class Atest {
+
+	public static void main(String[] args) {
+		System.out.println("conflict");
+	}
+
+}
---
+++ src/ExceptionClasses/Btest.java
@@ -0,0 +1,8 @@
+package ExceptionClasses;
+
+public class Btest {
+	public static void main(String[] args) {
+		System.out.println("conflict");
+	}
+
+}
